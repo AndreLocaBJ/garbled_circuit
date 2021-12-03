74fbbd4e97fbfb0ebecd3aeee270271d3fff17c7
Andrea Locatelli <ciao.loca@gmail.com>
2021-12-03 14:03:44 +0100

    FINE

--- scritto/_build.sh
+++ scritto/_build.sh
@@ -6,3 +6,4 @@ set -ev
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 #Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
 
+pdfunite CopertinaTesi.pdf ./_book/bookdown-demo.pdf tesi.pdf
